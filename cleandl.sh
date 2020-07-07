d1c601e20f165a22b8e4738eaa2ae7e176a59dbc
Alexander <7086600@gmail.com>
2020-07-07 22:39:56 +0300

    Add function

--- cleandl.sh
+++ cleandl.sh
@@ -21,63 +21,18 @@ DISTR=$(ls *.dmg *.pkg *.exe) #дистрибутивы
 #Internal Field Separator. Оставляем в качестве разделителя полей только перевод строки
 IFS=$'\n'
 
-# перемещаем все картинки в папку img
-for file_img in $IMG
+# функция для перемещения файлов по папкам
+function cleanDl {
+for file in $IMG
 do
-  mv $file_img img/
+  mv $file img/
 done
+}
 
-# перемещаем все документы в папку docs
-for file_doc in $DOCS
-do
-  mv $file_doc docs/
-done
-
-# перемещаем все таблички в папку xls
-for file_t in $XLS
-do
-  mv $file_t xls/
-done
-
-# перемещаем все csv в папку csv
-for file_csv in $CSV
-do
-  mv $file_csv csv/
-done
-
-# перемещаем все pdf в папку pdf
-for file_pdf in $PDF
-do
-  mv $file_pdf pdf/
-done
-
-# перемещаем все аудио и видео в папку media
-for file_media in $MEDIA
-do
-  mv $file_media media/
-done
-
-# перемещаем все архивы в папку arch
-for file_arch in $ARCH
-do
-  mv $file_arch arch/
-done
-
-# перемещаем все книги  в папку books
-for file_book in $BOOKS
-do
-  mv $file_book books/
-done
-
-# перемещаем все дистрибутивы  в папку distr
-for file_distr in $DISTR
-do
-  mv $file_distr distr/
-done
+cleanDl
 
 echo
 echo "Clean Dowloads folder complete"
-
 pwd
 
 echo
